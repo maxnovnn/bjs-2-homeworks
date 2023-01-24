f57da0a2475728ef23872e89e466357d17e38e93
Максим Новожилов <intention-m26@mail.ru>
2023-01-24 10:22:00 +0300

    Initial commit

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,27 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
+  let x1;
+  let x2;
+  let d = Math.pow(b, 2) - 4*a*c;
   
+  if (d === 0) {
+      x1 =  -b/(2 * a);
+      arr.push(x1);
+  };
+    
+  if (d > 0) {
+      x1 = (-b + Math.sqrt(d))/(2 * a);
+      x2 = (-b - Math.sqrt(d))/(2 * a);
+      arr.push(x1, x2);
+  }
   return arr;
 }
 
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+function calculateTotalMortgage(percent, contribution, amount, date) {
+  let totalAmount;
+
+  // код для задачи №2 писать здесь
+
+  return totalAmount;
 }
