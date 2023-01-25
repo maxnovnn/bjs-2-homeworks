06168e7c3307b3d0b89aa4599960f17a4a31d099
Максим Новожилов <intention-m26@mail.ru>
2023-01-25 11:12:58 +0300

    update 2.functions

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,24 +1,55 @@
-function getArrayParams(...arr) {
+// Задание 1
+function getArrayParams(arr) {
+  let min = Infinity;
+  let max = -Infinity;
+  let sum = 0;
+  let avg;
+
+  for (let i = 0; i <  arr.length; i++) {
+    if (arr[i] > max)
+    max = arr[i];
+    if (arr[i] < min)
+    min = arr[i];
+    
+    sum += arr[i]; 
+  }
+
+  avg = Number((sum / arr.length).toFixed(2));
 
   return { min: min, max: max, avg: avg };
 }
 
-function summElementsWorker(...arr) {
-
+// Задание 2
+function worker(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum = sum + arr[i];
+  }
+  return sum;
 }
 
-function differenceMaxMinWorker(...arr) {
-
+function makeWork(arrOfArr, func) {
+  let max = -Infinity;
+  for (let i = 0; i < arrOfArr.length; i++) {
+    let arrSum = func(arrOfArr[i]);
+    if (arrSum > max) {
+      max = arrSum;
+    }
+  }
+  return max;
 }
 
-function differenceEvenOddWorker(...arr) {
+// Задание 3
+function worker2(arr) {
+  let min = Infinity;
+  let max = -Infinity;
 
-}
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max)
+    max = arr[i];
+    if (arr[i] < min)
+    min = arr[i];
+  }
 
-function averageEvenElementsWorker(...arr) {
-
-}
-
-function makeWork (arrOfArr, func) {
-
+  return max - min;
 }
