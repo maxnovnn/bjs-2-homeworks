238d692cc956ef603c6c44960856f34f693dfc6e
Максим Новожилов <intention-m26@mail.ru>
2023-01-26 08:45:34 +0300

    update 3.arrays

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,16 @@
 function compareArrays(arr1, arr2) {
+    let result;
+    result = arr1.length === arr2.length && arr1.every((n, i) => n === arr2[i]);
   
+    return result; // boolean  
 }
 
-function getUsersNamesInAgeRange(users, gender) {
+function advancedFilter(arr) {
+    let resultArr;
+    resultArr = arr
+    .filter((element) => element > 0) 
+    .filter((element) => element % 3 === 0) 
+    .map((element) => element * 10); 
   
-}
+    return resultArr; // array
+  }
