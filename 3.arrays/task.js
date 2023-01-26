48c00af6905f8728e2cfc1ffd3242213e3d03fe5
Максим Новожилов <intention-m26@mail.ru>
2023-01-26 10:09:31 +0300

    second commit

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,16 +1,7 @@
 function compareArrays(arr1, arr2) {
-    let result;
-    result = arr1.length === arr2.length && arr1.every((n, i) => n === arr2[i]);
-  
-    return result; // boolean  
+    return arr1.length === arr2.length && arr1.every((n, i) => n === arr2[i]);
 }
 
 function advancedFilter(arr) {
-    let resultArr;
-    resultArr = arr
-    .filter((element) => element > 0) 
-    .filter((element) => element % 3 === 0) 
-    .map((element) => element * 10); 
-  
-    return resultArr; // array
-  }
+    return arr.filter(element => element > 0 && element % 3 === 0).map(element => element *10);
+}
