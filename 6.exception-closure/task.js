ce7c866f05df3d42ae41634321ef979b322575ba
Максим Новожилов <intention-m26@mail.ru>
2023-01-29 22:25:52 +0300

    update 6.exception-closure

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,58 @@
-﻿
+﻿//Задача 1.
+function parseCount (value) {
+    const number = Number.parseInt(value);
+    if (isNaN (number)) {
+        throw new Error ('Невалидное значение');        
+    } 
+    return number;
+}     
+
+function validateCount (value) {
+    try {
+        return parseCount (value);
+    }
+    catch (error) {
+        return error;
+    }
+}
+
+
+//Задача 2.
+class Triangle {
+    constructor(a, b, c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    
+        if (a + b < c || a + c < b || b + c < a) {
+           throw new Error ('Треугольник с такими сторонами не существует');
+        }       
+    }
+
+
+getPerimeter() {
+    return this.a + this.b + this.c;
+    }
+
+getArea() {
+    const p = this.getPerimeter() / 2; 
+    return Number(Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c)).toFixed(3));
+    }
+
+}
+
+function getTriangle(a, b, c) {
+    try {
+        return new Triangle(a, b, c);
+    }
+    catch (error) {
+        return {
+            getPerimeter() {
+                return 'Ошибка! Треугольник не существует';
+            },
+            getArea() {
+                return 'Ошибка! Треугольник не существует';
+            }
+        }
+    }
+}
