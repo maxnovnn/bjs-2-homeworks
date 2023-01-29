1a4e24db6b82300c6272f85852820ba151653a89
Максим Новожилов <intention-m26@mail.ru>
2023-01-29 23:23:13 +0300

    update 8.decorators

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,47 @@
-//Задача № 1
 function cachingDecoratorNew(func) {
-  
+  let cache = [];  
+    function wrapper(...args) {
+      const hash = args.join(',');
+      let idx = cache.findIndex((item) => item.hash == hash);
+      if (idx !== -1) {
+        console.log("Из кэша: " + cache[idx].result);
+        return "Из кэша: " + cache[idx].result;
+      }
+    
+      let result = func(...args);
+      cache.push({hash, result});
+      if (cache.length > 5) {
+        cache.shift();
+      }
+      console.log("Вычисляем: " + result);
+      return "Вычисляем: " + result;
+    }
+    return wrapper;
 }
-
-//Задача № 2
-function debounceDecoratorNew(func, delay) {
   
+function debounceDecoratorNew(func, ms) {
+  let timer = null;  
+    function wrapper(...args) {
+      if (timer === null) {
+        func(...args);
+      }
+      clearTimeout(timer);
+      timer = setTimeout(() => func(...args), ms);
+    }
+    return wrapper;
+}
+  
+function debounceDecorator2(func, ms) {
+  let timer = null;
+  
+    function wrapper(...args) {
+      if (timer === null) {
+        func(...args);
+      }
+      clearTimeout(timer);
+      timer = setTimeout(() => func(...args), ms);
+      wrapper.count++;
+    }
+    wrapper.count = 0;
+    return wrapper;
 }
